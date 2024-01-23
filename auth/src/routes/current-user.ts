ef30adb1b4d938b7bdb05357642e4ff0150b5e26
salkhon <salmankhondker1998@gmail.com>
2024-01-23 21:49:07 +0600

    auth service done

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there salkhon!")
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+	res.send({ currentUser: req.currentUser ?? null }); // currentUser is optional
 });
 
 export { router as currentUserRouter };
