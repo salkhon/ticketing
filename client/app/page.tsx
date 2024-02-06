fbce8b5e5c0e95a632c3f21049d55659131d2745
salkhon <salmankhondker1998@gmail.com>
2024-02-07 00:35:25 +0600

    created signupt frontend

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RootLayout from "./layout";
+
+export default function App() {
+	return (
+		<RootLayout>
+			<h1>My App</h1>
+			<p>Content</p>
+		</RootLayout>
+	);
+}
